6c6811692d03ad9e271ac190299a0f62402b6e4d
seestars <stars_dream@qq.com>
2020-08-27 12:57:07 +0800

    Chapter senven Update | 2020.08.27

--- Chapter senven/test.c
+++ Chapter senven/test.c
@@ -1,11 +1,34 @@
-// test.c —— 测试用
+// test.c —— 用户输入单词，并打印一遍，并智能去除无用占位符
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 int main (void)
 {
-    int test1 = 0;
-    float test2 = 0.0;
-    
-    printf("test1 is %d, test2 is %f", 1/test1, 1/test2);
+    char test1;
+    bool inword = false;
+
+    printf("Enter a char: \n");
+    while ((test1 = getchar()) != '#')
+    {
+        if (!isspace(test1) && !inword)
+        {
+            inword = true;
+            printf("Check it: ");
+        }
+        else
+        {
+            if (isspace(test1) && inword)
+                inword = false;
+        }
+        if (isspace(test1))
+        {
+            test1 = ' ';
+            if (!inword)
+                printf("\b\n");
+        }
+        else 
+                putchar(test1);
+    }
     
     return 0;
 }
